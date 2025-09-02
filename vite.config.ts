3fd6f325e0ce688d7cb88ed51835d237b8da6287
hassan-boulhilt <6063121+Hassan-Boulhilt@users.noreply.github.com>
2025-09-03 00:58:35 +0100

    change vite.config.js file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [cloudflare()],
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  plugins: [
+    react(),
+    cloudflare()
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+

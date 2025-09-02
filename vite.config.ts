0790d4e05fabf5c6833fbb9de5c5feca8eb8d6e6
hassan-boulhilt <6063121+Hassan-Boulhilt@users.noreply.github.com>
2025-09-03 00:33:11 +0100

    add wrangler.toml and configure it in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,8 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react()
+    react(),
+    cloudflare()
   ].filter(Boolean),
   resolve: {
     alias: {

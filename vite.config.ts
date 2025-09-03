049353698824ec0999419cbbd2bf56af026586f9
hassan-boulhilt <6063121+Hassan-Boulhilt@users.noreply.github.com>
2025-09-03 03:16:27 +0100

    change vite.config.js file

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
+    cloudflare(),
     react(),
-    cloudflare()
+    
   ].filter(Boolean),
   resolve: {
     alias: {
